8ff26184c0af46ba0091946398d92456b0689969
ltimaginea <ltimaginea@163.com>
2021-03-19 00:21:01 +0800

    bind与重载函数

---
+++ CppPrimer/Content/Ch10_GenericAlgorithm/Ch10_04_bind_03.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+#include <functional>
+
+using namespace std;
+
+void Goodbye(const string& name)
+{
+    cout << name << ",goodbye!" << endl;
+}
+
+void Goodbye(const string& name1, const string& name2)
+{
+    cout << name1 << "," << name2 << ",goodbye!" << endl;
+}
+
+int main()
+{
+    using namespace std::placeholders;
+
+    using Goodbye1ParamType = void(*)(const string& name);
+    using Goodbye2ParamType = void(*)(const string& name1, const string& name2);
+    
+    auto f1 = bind(static_cast<Goodbye1ParamType>(Goodbye), _1);
+    auto f2 = bind(static_cast<Goodbye2ParamType>(Goodbye), _1, _2);
+    
+    f1("Scott Meyers");
+    f2("Scott Meyers", "Herb Sutter");
+    
+    return 0;
+}
+
+// tips: 
+//   1. bind 无法直接使用重载函数，需要强制转型到适当的函数指针型别。
