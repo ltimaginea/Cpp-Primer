92fecb7d1a90b3230ba1dfbe97cfe9c6e20a701a
ltimaginea <ltimaginea@163.com>
2021-03-18 22:30:49 +0800

    bind

---
+++ CppPrimer/Content/Ch10_GenericAlgorithm/Ch10_04_bind_00.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <functional>
+
+using namespace std;
+
+bool CheckSize(const string& str, string::size_type size)
+{
+    if (str.size() >= size)
+        return true;
+    else
+        return false;
+}
+
+// words按字典序排序，并去重
+void ElimDups(vector<string>& words)
+{
+    sort(words.begin(), words.end());
+    auto end_unique = unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
+
+// words按长度排序，长度相同的单词维持字典序。计算大于等于指定长度的元素数目并打印对应单词
+void Biggies(vector<string>& words, string::size_type size)
+{
+    ElimDups(words);
+    stable_sort(words.begin(), words.end(), [](const string& a, const string& b) {return a.size() < b.size(); });
+    
+    using namespace std::placeholders;
+    auto it = find_if(words.begin(), words.end(), bind(CheckSize, _1, size));
+    
+    auto cnt = words.end() - it;
+    for_each(it, words.end(), [](const string& str) {cout << str << " "; });
+}
+
+int main()
+{
+    vector<string> words = { "the","quick","red","fox","jumps","over","the","slow","red","turtle" };
+    Biggies(words, 4);
+    return 0;
+}
