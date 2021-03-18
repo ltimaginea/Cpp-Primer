0ea090ce3389d3d15040f8ebb38061be5341753d
ltimaginea <ltimaginea@163.com>
2021-03-18 23:45:44 +0800

    绑定引用参数

---
+++ CppPrimer/Content/Ch10_GenericAlgorithm/Ch10_04_bind_02.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <functional>
+
+using namespace std;
+
+ostream& Print(ostream& os, const string& str, char ch)
+{
+    return os << str << ch;
+}
+
+// words按字典序排序，并去重
+void ElimDups(vector<string>& words)
+{
+    sort(words.begin(), words.end());
+    auto end_unique = unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
+
+// words按长度排序，长度相同的单词维持字典序。计算大于等于指定长度的元素数目并打印对应单词
+void Biggies(vector<string>& words, string::size_type size)
+{
+    ElimDups(words);
+    stable_sort(words.begin(), words.end(), [](const string& a, const string& b) {return a.size() < b.size(); });
+    auto it = find_if(words.begin(), words.end(), [size](const string& str) {return str.size() >= size; });
+    auto cnt = words.end() - it;
+
+    using namespace std::placeholders;
+    for_each(it, words.end(), bind(Print, ref(cout), _1, ' '));
+}
+
+int main()
+{
+    vector<string> words = { "the","quick","red","fox","jumps","over","the","slow","red","turtle" };
+    Biggies(words, 4);
+    return 0;
+}
+
+// tips: 
+//   1. 实参 形参 值传递 引用传递 ???
