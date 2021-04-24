371cd6fa5597d191bbd273fd1803e6800472e8e1
ltimaginea <ltimaginea@163.com>
2021-04-25 00:40:06 +0800

    fill_n

---
+++ CppPrimer/Content/Ch10_GenericAlgorithm/Ch10_01_fill_n.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v(10, 233);
+
+	auto n = v.size() / 2;
+	fill_n(v.begin(), n, -1);		// 从目的位置开始的序列应至少包含n个元素，要能容纳要写入的元素
+	for (auto elem : v)
+		cout << elem << " " << endl;
+	
+	return 0;
+}
+
+// tips:
+//	1. 从目的位置开始的序列应至少包含n个元素，要能容纳要写入的元素。
