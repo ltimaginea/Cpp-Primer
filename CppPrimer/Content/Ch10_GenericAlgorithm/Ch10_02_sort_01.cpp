d0589f3926091c6a7100e1b865366f8661da62f8
ltimaginea <ltimaginea@163.com>
2021-04-24 23:43:34 +0800

    二级排序

---
+++ CppPrimer/Content/Ch10_GenericAlgorithm/Ch10_02_sort_01.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct node
+{
+	int x;
+	int y;
+}ssd[3];
+
+bool cmp(node a, node b)
+{
+	if (a.x != b.x)
+		return a.x > b.x;
+	else
+		return a.y > b.y;
+}
+
+int main()
+{
+	ssd[0].x = 2; ssd[0].y = 2;		// [2,2]
+	ssd[1].x = 1; ssd[1].y = 3;		// [1,3]
+	ssd[2].x = 2; ssd[2].y = 1;		// [2,1]
+	sort(ssd, ssd + 3, cmp);
+	for (int i = 0; i < 3; ++i)
+	{
+		cout << ssd[i].x << " " << ssd[i].y << endl;
+	}
+
+	return 0;
+}
+
+
+
+// tips:
+//	1. 常量对象，以及常量对象的引用或指针都只能调用常量成员函数。
