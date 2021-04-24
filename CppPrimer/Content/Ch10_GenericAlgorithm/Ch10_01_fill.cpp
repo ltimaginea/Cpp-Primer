60522f13061c50536f576ff0b50f011d8fbee475
ltimaginea <ltimaginea@163.com>
2021-04-25 00:18:38 +0800

    fill

---
+++ CppPrimer/Content/Ch10_GenericAlgorithm/Ch10_01_fill.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v(10);
+	
+	fill(v.begin(), v.end(), 233);
+	for (auto elem : v)
+		cout << elem << " " << endl;
+
+	fill(v.begin(), v.end() - v.size() / 2, -1);
+	for (auto elem : v)
+		cout << elem << " " << endl;
+	
+	return 0;
+}
+
+// tips:
+//	1. 常量对象，以及常量对象的引用或指针都只能调用常量成员函数。
