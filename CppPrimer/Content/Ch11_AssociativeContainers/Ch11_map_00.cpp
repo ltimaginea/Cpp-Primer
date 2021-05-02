73f05aca4e55b41e95589970b7bdc3499ce74b6c
ltimaginea <ltimaginea@163.com>
2021-05-02 23:07:56 +0800

    map

---
+++ CppPrimer/Content/Ch11_AssociativeContainers/Ch11_map_00.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	map<string, size_t> word_count = { {"the",11},{"but",31},{"and",7},{"or",23},{"an",12},{"a",1} };
+	string word;
+	while (cin >> word)
+	{
+		auto ret = word_count.insert({ word,1 });
+		if (!ret.second)
+			++word_count[word];
+	}
+	for (auto m : word_count)
+		cout << m.first << "\t" << m.second << endl;
+	return 0;
+}
