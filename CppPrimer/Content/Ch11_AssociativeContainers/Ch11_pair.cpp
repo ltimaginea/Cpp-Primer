f830e2eeda1ab6968423ad84fdbbce06476a4b0d
ltimaginea <ltimaginea@163.com>
2021-05-06 21:32:11 +0800

    pair

---
+++ CppPrimer/Content/Ch11_AssociativeContainers/Ch11_pair.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+	vector<pair<string, int>> vec;
+	string str;
+	int i;
+
+	while (cin >> str >> i)
+	{
+		vec.push_back(pair<string, int>(str, i));
+		vec.push_back(make_pair(str, i));
+		vec.push_back({ str,i });		// 用花括号包围的初始化器来返回pair类型对象
+		vec.emplace_back(str, i);
+		vec.push_back(vector<pair<string, int>>::value_type(str, i));
+	}
+
+	for (const auto& p : vec)
+		cout << p.first << ":" << p.second << endl;
+
+	return 0;
+}
+
+// tips: 
+//   1. pair的数据成员first和second是public的。
