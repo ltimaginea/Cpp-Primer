3540f976dba78fa2d6abccae0cde9a4e2d78bf6d
ltimaginea <ltimaginea@163.com>
2021-05-30 00:00:28 +0800

    use_count

--- CppPrimer/Content/Ch12_DynamicMemory/Ch12_shared_ptr_01.cpp
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_shared_ptr_01.cpp
@@ -3,22 +3,22 @@
 
 void process(std::shared_ptr<int> ptr)
 {
-    std::cout << "inside the process function:" << ptr.use_count() << "\n";
+	std::cout << "inside the process function:" << ptr.use_count() << "\n";
 }
 
 int main()
 {
-    std::shared_ptr<int> p(new int(42));
-    process(std::shared_ptr<int>(p));
+	std::shared_ptr<int> p(new int(42));
+	process(std::shared_ptr<int>(p));
 
-    /**
-    * codes below shows how the reference count change.
-    */
-    std::cout << p.use_count() << "\n";
-    auto q = p;
-    std::cout << p.use_count() << "\n";
-    std::cout << "the int p now points to is:" << *q << "\n";
-    return 0;
+	/**
+	* codes below shows how the reference count change.
+	*/
+	std::cout << p.use_count() << "\n";
+	auto q = p;
+	std::cout << p.use_count() << "\n";
+	std::cout << "the int p now points to is:" << *q << "\n";
+	return 0;
 }
 
 /* Output:
