f38037db3f02a73e6e47939e54893979e4213bc0
ltimaginea <ltimaginea@163.com>
2021-05-30 17:31:05 +0800

    new get shared_ptr

--- CppPrimer/Content/Ch12_DynamicMemory/Ch12_shared_ptr_03.cpp
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_shared_ptr_03.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <memory>
 
-
 void process(std::shared_ptr<int> ptr)
 {
 	std::cout << "inside the process function:" << ptr.use_count() << "\n";
@@ -11,20 +10,30 @@ void process(std::shared_ptr<int> ptr)
 
 int main()
 {
-	auto p = new int();
-	auto sp = std::make_shared<int>();
+	auto p = new int(42);
+	auto sp = std::make_shared<int>(422);
 
-	// legal. Just copy sp which is a shared_ptr to process().
+	// Legal. Just copy sp which is a shared_ptr to process().
 	// process(sp);
 
-	// illegale.plain pointer can not convert to smart pointer implicitly.
+	// Illegale. Plain pointer can not convert to smart pointer implicitly.
 	// process(new int());
 
-	// illegale.plain pointer can not convert to smart pointer implicitly.
+	// Illegale. Plain pointer can not convert to smart pointer implicitly.
 	// process(p);
 
-	// Legal. But it's a bad practice to do so.Because using smart pointer together with raw pointer could potentially cause problems.
+	// Legal. But memory will be freed, it's a bad practice to do so.Because using smart pointer together with raw pointer could potentially cause problems.
 	// process(std::shared_ptr<int>(p));
+	// Undefined: p is a dangling pointer.
+	// int j = *p;
+
+	// Illegale. std::shared_ptr<int>(sp.get()) construct a temporary shared_ptr and copy it to the parameter.
+	// However it is not a copy of sp.As a result, at end of this main function sp will free the memory that has been freed inside process().
+	// That's why "double freed or corruption" was generated.
+	// process(std::shared_ptr<int>(sp.get()));
+
+	// Illegale. Generate a runtime error : double free or corruption
+	// delete sp.get();
 
 	return 0;
 }
