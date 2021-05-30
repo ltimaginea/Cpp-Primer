25cb3f65c16c185634e2d108877a35302735c560
ltimaginea <ltimaginea@163.com>
2021-05-31 02:38:27 +0800

    make_unique

---
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_unique_ptr_03.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <memory>
+
+// C++11 手动实现一个基础功能版 make_unique ,这个基础函数不支持数组和自定义删除器
+template <typename T, typename... Ts>
+std::unique_ptr<T> make_unique(Ts&&... args)
+{
+	return std::unique_ptr<T>{new T{ std::forward<Ts>(args)... }};
+}
+
+// tips: 
+//   1. 从这个基础函数可以看出，make 函数把实参完美转发给构造函数并返回构造出的智能指针
