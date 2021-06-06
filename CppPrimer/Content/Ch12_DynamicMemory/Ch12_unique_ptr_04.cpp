222919083db0f07419e4ab74359c6855d6d1322b
ltimaginea <ltimaginea@163.com>
2021-06-07 02:08:15 +0800

    deleter

---
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_unique_ptr_04.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Connection
+{
+	Connection(string ip, short port) :ip_(ip), port_(port) {}
+	string ip_;
+	short port_;
+};
+
+struct Destination
+{
+	Destination(string ip, short port) :ip_(ip), port_(port) {}
+	string ip_;
+	short port_;
+};
+
+Connection Connect(Destination des)
+{
+	Connection conn = Connection(des.ip_, des.port_);
+	cout << "create connection:\t" << conn.ip_ << ":" << conn.port_ << endl;
+	return conn;
+}
+
+void Disconnect(Connection conn)
+{
+	cout << "close connection:\t" << conn.ip_ << ":" << conn.port_ << endl;
+}
+
+void EndConnection(Connection* ptr)
+{
+	Disconnect(*ptr);
+}
+
+void Foo(Destination des)
+{
+	Connection conn = Connect(des);
+	//unique_ptr<Connection, decltype(EndConnection)*> uptr(&conn, EndConnection);
+	auto del = [](Connection* ptr) { Disconnect(*ptr); };
+	unique_ptr<Connection, decltype(del)> uptr(&conn, del);
+	cout << "connecting now:\t\t" << conn.ip_ << ":" << conn.port_ << endl;
+}
+
+int main()
+{
+	Destination des("202.118.176.67", 3316);
+	Foo(des);
+
+	auto del = [](int* p) { delete p; cout << "deleter" << endl; };
+	unique_ptr<int, decltype(del)> sp(new int(42), del);
+
+	return 0;
+}
+
+/* 输出：
+create connection:      202.118.176.67:3316
+connecting now:         202.118.176.67:3316
+close connection:       202.118.176.67:3316
+deleter
+*/
+
+// tips: 
+//   1. shared_ptr自定义删除器需要单个shared_ptr内部指针类型的参数
