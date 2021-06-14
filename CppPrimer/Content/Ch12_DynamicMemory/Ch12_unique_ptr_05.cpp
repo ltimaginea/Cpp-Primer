8ab78242d3477e4cf9fba4f1c4ca2330324b9290
ltimaginea <ltimaginea@163.com>
2021-06-14 23:52:40 +0800

    unique_ptr<T[]>

---
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_unique_ptr_05.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+	// up指向一个包含10个未初始化int的数组
+	unique_ptr<int[]> up(new int[10]);
+	for (size_t i = 0; i != 10; ++i)
+	{
+		// 用下标运算符来访问数组中的元素
+		up[i] = i;
+	}
+	// 自动用 delete[] 销毁其指针
+	up.reset();
+
+	return 0;
+}
+
+// tips: 
+//   1. 为了用一个unique_ptr管理动态数组，我们必须在对象类型后面跟一对空方括号[]，即 unique_ptr<T[]>
