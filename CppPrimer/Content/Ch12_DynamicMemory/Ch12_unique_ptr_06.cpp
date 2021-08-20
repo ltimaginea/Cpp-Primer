d3b043c0567a972cdbc2bc2774f44b92a1292e2b
ltimaginea <ltimaginea@163.com>
2021-08-20 14:40:32 +0800

    operator=   make_unique

--- CppPrimer/Content/Ch12_DynamicMemory/Ch12_unique_ptr_06.cpp
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_unique_ptr_06.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <memory>
 
@@ -8,6 +9,39 @@ struct Foo
 	Foo(int id = 0) : id(id) { std::cout << "Foo " << id << '\n'; }
 	~Foo() { std::cout << "~Foo " << id << '\n'; }
 };
+
+class Song
+{
+public:
+	std::wstring title_;
+	std::wstring artist_;
+
+	Song(std::wstring title, std::wstring artist) : title_(title), artist_(artist) {  }
+	Song(std::wstring title) : title_(title), artist_(L"Unknown") {  }
+};
+
+void SongVector()
+{
+	using std::vector;
+	using std::unique_ptr;
+	using std::make_unique;
+
+	vector<unique_ptr<Song>> songs;
+
+	// Create a few new unique_ptr<Song> instances and add them to vector using implicit move semantics.
+	songs.push_back(make_unique<Song>(L"B'z", L"Juice"));
+	songs.push_back(make_unique<Song>(L"Namie Amuro", L"Funky Town"));
+	songs.push_back(make_unique<Song>(L"Kome Kome Club", L"Kimi ga Iru Dake de"));
+	songs.push_back(make_unique<Song>(L"Ayumi Hamasaki", L"Poker Face"));
+
+	// In the range for loop, notice that the unique_ptr is passed by reference. 
+	// If you try to pass by value here, the compiler will throw an error because the unique_ptr copy constructor is deleted.
+	// Pass by const reference when possible to avoid copying.
+	for (const auto& song : songs)
+	{
+		std::wcout << L"Artist: " << song->artist_ << L"   Title: " << song->title_ << std::endl;
+	}
+}
 
 int main()
 {
@@ -31,7 +65,7 @@ int main()
 	//up5.reset(new string(*up3));		// 同上等效
 
 	// 不建议下面这样赋值，其安全性依赖于*up的类型的operator=的实现，安全性不确定
-	*up1 = *up5;
+	// *up1 = *up5;
 
 	unique_ptr<Foo> p1(make_unique<Foo>(1));
 
@@ -45,7 +79,7 @@ int main()
 		// Foo instance will continue to live, despite p2 going out of scope
 	}
 
-	// create a unique_ptr to an array of 5 Foos.
+	// Create a unique_ptr to an array of 5 Foos.
 	unique_ptr<Foo[]> p_array = make_unique<Foo[]>(5);
 
 	{
@@ -54,7 +88,22 @@ int main()
 		std::cout << "About to leave inner block []...\n";
 	}
 
-	//p1 = std::move(p_array);
+	// 错误！类型不匹配
+	// p1 = std::move(p_array);
+
+	SongVector();
+
+	// We can use make_unique to create a unique_ptr to an array, but you cannot use make_unique to initialize the array elements.
+	// We have to initialize the array elements separately. Rather than using make_unique<T[]>, perhaps a better choice is to use a std::vector.
+	// Create a unique_ptr to an array of 5 integers.
+	auto p = make_unique<int[]>(5);
+	// Initialize the array elements separately.
+	for (int i = 0; i < 5; ++i)
+	{
+		p[i] = i;
+		std::cout << p[i] << " ";
+	}
+	std::cout << "\n";
 
 	std::cout << "About to leave program...\n";
 	return 0;
@@ -85,6 +134,11 @@ Foo 0
 ~Foo 0
 ~Foo 0
 About to leave inner block []...
+Artist: Juice   Title: B'z
+Artist: Funky Town   Title: Namie Amuro
+Artist: Kimi ga Iru Dake de   Title: Kome Kome Club
+Artist: Poker Face   Title: Ayumi Hamasaki
+0 1 2 3 4
 About to leave program...
 ~Foo 0
 ~Foo 0
