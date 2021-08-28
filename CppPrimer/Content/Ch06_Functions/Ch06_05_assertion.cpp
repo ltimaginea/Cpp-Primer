a20d7e7350cad52d0ad34fbebb772d2afec6f3de
ltimaginea <ltimaginea@163.com>
2021-08-28 22:42:03 +0800

    assertion

---
+++ CppPrimer/Content/Ch06_Functions/Ch06_05_assertion.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+// uncomment to disable assert()
+//#define NDEBUG
+#include <cassert>
+
+int main()
+{
+	// MSVC 和 MinGW-W64 都有预定义宏 _WIN32
+#ifdef _WIN32
+	std::cout << __cplusplus << std::endl;
+#endif // _WIN32
+
+	// MSVC 专用预定义宏
+#ifdef _MSC_VER
+	std::cout << _MSC_VER << std::endl;
+#endif // _MSC_VER
+
+	// linux-gnu g++ 专用预定义宏
+#ifdef __GNUG__
+	std::cout << __GNUG__ << __GNUC_MINOR__ << __GNUC_PATCHLEVEL__ << std::endl;
+#endif // __GNUG__
+
+	constexpr int i = 123;
+	constexpr int j = 456;
+	assert(i > j);
+	static_assert(i > j, "i > j failed");
+
+#ifndef NDEBUG
+	std::cout << "i = " << i
+		<< " j = " << j
+		<< " : in function " << __func__
+		<< std::endl;
+#endif // !NDEBUG
+
+	return 0;
+}
+
+// Tips:
+//	1. 默认状态下没有定义NDEBUG，此时assert将执行运行时检查
