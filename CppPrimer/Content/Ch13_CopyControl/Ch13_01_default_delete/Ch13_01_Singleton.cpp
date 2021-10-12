cb7cdcbd3df6dd7da047e5441905d35631c892ab
ltimaginea <ltimaginea@163.com>
2021-10-12 22:43:31 +0800

    default delete

---
+++ CppPrimer/Content/Ch13_CopyControl/Ch13_01_default_delete/Ch13_01_Singleton.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cassert>
+
+class Singleton
+{
+public:
+	static Singleton& Instance()
+	{
+		static Singleton instance;
+		return instance;
+	}
+public:
+	Singleton(const Singleton&) = delete;
+	Singleton(Singleton&&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+	Singleton& operator=(Singleton&&) = delete;
+private:
+	Singleton() = default;
+	~Singleton() = default;
+};
+
+int main()
+{
+	auto& a = Singleton::Instance();
+	auto& b = Singleton::Instance();
+	assert(&a == &b);
+
+	return 0;
+}
