8d5aaa94612068513c90cf89718c9898e1196486
ltimaginea <ltimaginea@163.com>
2021-10-30 20:10:10 +0800

    new_RawPointer_AvoidMemoryLeak

---
+++ CppPrimer/Content/Ch12_DynamicMemory/Ch12_new_RawPointer_AvoidMemoryLeak.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <new>
+
+class Widget { double d[1024]; /* ... */ };
+
+void Foo()
+{
+	Widget* ptr = new Widget;
+
+	try
+	{
+		// perform some operations...(may throw exceptions)
+		std::string().at(1);	// this generates an std::out_of_range
+	}
+	catch (...)			// for any exception
+	{
+		delete ptr;		// clean up
+		throw;			// rethrow the exception
+	}
+
+	delete ptr;			// clean up on normal end
+}
+
+int main()
+{
+	try
+	{
+		Foo();
+	}
+	catch (const std::bad_alloc& err)
+	{
+		std::cout << err.what() << std::endl;
+	}
+	catch (const std::exception& err)
+	{
+		std::cout << err.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "unknown exceptions" << std::endl;
+	}
+
+	return 0;
+}
