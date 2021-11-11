d30e6e1490d5887710f300fa45fdd2d4a740f6d5
ltimaginea <ltimaginea@163.com>
2021-11-11 22:05:25 +0800

    AccessControl_Inheritance

--- CppPrimer/Content/Ch15_ObjectOrientedProgramming/Ch15_05_AccessControl_Inheritance.cpp
+++ CppPrimer/Content/Ch15_ObjectOrientedProgramming/Ch15_05_AccessControl_Inheritance.cpp
@@ -24,6 +24,8 @@ private:
 	int private_;
 };
 
+//class Derived : private Base
+//class Derived : protected Base
 class Derived : public Base
 {
 public:
@@ -68,10 +70,10 @@ void Base::F(Base& b, Derived& d)
 	++b.protected_;
 	++b.public_;
 
-	++d.public_;		// OK!!!
-	++d.protected_;		// OK!!!
-	++d.private_;		// OK!!!
-	++d.d_public_;		// OK!!!
+	++d.public_;		// 经过测试发现仅公有继承时才可以访问该成员
+	++d.protected_;		// 经过测试发现仅公有继承时才可以访问该成员
+	++d.private_;		// 经过测试发现仅公有继承时才可以访问该成员
+	++d.d_public_;
 	//++d.d_protected_;
 	//++d.d_private_;
 }
@@ -89,11 +91,15 @@ int main()
 	Derived d;
 	//int n = b.protected_;		// 错，在类的成员函数外部，不能访问基类保护成员
 	//n = d.private_;			// 错，此处不能访问d的私有成员
-	d.public_ = 10;				// OK
+	d.public_ = 10;				// 仅公有继承时可以访问
+	d.d_public_ = 11;			// OK
 
 	return 0;
 }
 
 // Tips:
-//	1. 在类的成员函数内部可以访问同类其他对象的全部(私有, 保护, 公有)成员(不含基类的私有成员，其是不可访问的)。
-//	2. 需要留意基类 Base 的成员函数 Base::F 对普通的派生类对象的成员的访问权限。
+// 1. 在类的成员函数内部可以访问同类其他对象的全部(私有, 保护, 公有)成员(不含继承的基类的私有成员，其是不可访问的)。
+// 2. 需要留意基类Base的成员函数Base::F对普通的派生类对象的成员的访问权限：
+// 按理说，基类的成员和友元对于普通的派生类对象，也应该是不具有特殊的访问权限的。
+// 但经过测试发现，对于protected或private继承的方式，基类的成员和友元是符合预期地仅可以访问普通派生类对象自定义部分的public成员；
+// 但是对于public继承的方式，基类的成员和友元是出乎预期地既可以访问普通派生类对象自定义部分的public成员，还可以访问普通派生类对象继承自基类部分的所有成员。
