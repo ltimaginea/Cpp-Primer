46f24cff9efb195c12e372d5ffa54277bf28f1a6
ltimaginea <ltimaginea@163.com>
2021-11-18 22:29:34 +0800

    access to virtual functions

---
+++ CppPrimer/Content/Ch15_ObjectOrientedProgramming/Ch15_05_access_to_virtual_functions.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <memory>
+
+class VFuncBase
+{
+public:
+	VFuncBase() = default;
+	VFuncBase(const VFuncBase&) = default;
+	VFuncBase(VFuncBase&&) = default;
+	VFuncBase& operator=(const VFuncBase&) = default;
+	VFuncBase& operator=(VFuncBase&&) = default;
+	virtual ~VFuncBase() = default;
+public:
+	virtual int GetState() { return state_; }
+protected:
+	int state_ = 0;
+};
+
+class VFuncDerived : public VFuncBase
+{
+public:
+	VFuncDerived() = default;
+	VFuncDerived(const VFuncDerived&) = default;
+	VFuncDerived(VFuncDerived&&) = default;
+	VFuncDerived& operator=(const VFuncDerived&) = default;
+	VFuncDerived& operator=(VFuncDerived&&) = default;
+	~VFuncDerived() override = default;
+private:
+	int GetState() override { return state_; }
+};
+
+int main()
+{
+	int state{ 0 };
+	VFuncDerived vfd;			// Object of derived type.
+	VFuncBase& rvfb = vfd;		// Reference to base type.
+	VFuncDerived& rvfd = vfd;	// Reference to derived type.
+
+	state = rvfb.GetState();	// GetState is public.
+	//state = rvfd.GetState();	// error! GetState is private.
+
+	std::unique_ptr<VFuncBase> ptr_base = std::make_unique<VFuncDerived>();
+	std::unique_ptr<VFuncDerived> ptr_derived = std::make_unique<VFuncDerived>();
+
+	state = ptr_base->GetState();		// OK
+	//state = ptr_derived->GetState();	// error! GetState is private.
+
+	return 0;
+}
+
+// Tips:
+//	1. 一般情况下，基类与派生类的同名同参数表的虚函数的访问范围说明符应保持一致。
