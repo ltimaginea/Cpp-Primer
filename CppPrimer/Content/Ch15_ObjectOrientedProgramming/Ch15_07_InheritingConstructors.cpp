db0700f16398a25965cc614e824760e0a5dbb0be
ltimaginea <ltimaginea@163.com>
2021-11-26 17:39:48 +0800

    inheriting constructors

---
+++ CppPrimer/Content/Ch15_ObjectOrientedProgramming/Ch15_07_InheritingConstructors.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+class Base
+{
+public:
+	Base() = default;
+	Base(const Base&) = default;
+	Base(Base&&) = default;
+	Base& operator=(const Base&) = default;
+	Base& operator=(Base&&) = default;
+	virtual ~Base() = default;
+public:
+	Base(int i) : i_(i) {  }
+	Base(int i, double d) : i_(i), d_(d) {  }
+	Base(int i, double d, std::string s) : i_(i), d_(d), s_(s) {  }
+private:
+	int i_;
+	double d_;
+	std::string s_;
+};
+
+class Derived : public Base
+{
+public:
+	using Base::Base;	// inheriting constructors
+
+	Derived() = default;
+	Derived(const Derived&) = default;
+	Derived(Derived&&) = default;
+	Derived& operator=(const Derived&) = default;
+	Derived& operator=(Derived&&) = default;
+	~Derived() override = default;
+private:
+	int value_;
+};
+
+int main()
+{
+	Base b;
+	Derived d1(5);
+	Derived d2(5, 3.14);
+	Derived d3(5, 3.14, "yesterday's memory");	// 派生类自己的数据成员 Derived::value_ 将被默认初始化
+
+	return 0;
+}
