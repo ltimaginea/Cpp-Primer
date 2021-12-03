267cbb72fb625ee1e22ff4f29337788a2bd6f65d
ltimaginea <ltimaginea@163.com>
2021-12-03 23:59:20 +0800

    better HasPtrValueLike.cpp

--- CppPrimer/Content/Ch13_CopyControl/Ch13_02_HasPtrValueLike_better.cpp
+++ CppPrimer/Content/Ch13_CopyControl/Ch13_02_HasPtrValueLike_better.cpp
@@ -10,9 +10,9 @@ public:
 	// default constructor and constructor that takes a string
 	HasPtr(const std::string& s = std::string()) : ps_(new std::string(s)), d_(3.14) {  }
 	// copy constructor
-	HasPtr(const HasPtr&);
+	HasPtr(const HasPtr& rhp) : ps_(rhp.ps_ ? (new std::string(*rhp.ps_)) : nullptr), d_(rhp.d_) {  }
 	// move constructor
-	HasPtr(HasPtr&&) noexcept;
+	HasPtr(HasPtr&& rhp) noexcept : ps_(rhp.ps_), d_(rhp.d_) { rhp.ps_ = nullptr; }
 	// copy assignment operator
 	HasPtr& operator=(const HasPtr&);
 	// move assignment operator
@@ -30,36 +30,17 @@ private:
 	double d_;
 };
 
-// copy constructor
-HasPtr::HasPtr(const HasPtr& rhp) : ps_(nullptr), d_(rhp.d_)
-{
-	if (rhp.ps_)
-	{
-		ps_ = new std::string(*rhp.ps_);
-	}
-}
-
-// move constructor
-HasPtr::HasPtr(HasPtr&& rhp) noexcept : ps_(rhp.ps_), d_(rhp.d_)
-{
-	rhp.ps_ = nullptr;
-}
-
 // 既可以处理自赋值的情况，也还是异常安全的
 // copy assignment operator
 HasPtr& HasPtr::operator=(const HasPtr& rhp)
 {
-	std::string* newp = nullptr;
-	if (rhp.ps_)
-	{
-		// copy the underlying string
-		newp = new std::string(*rhp.ps_);
-	}
+	// if rhp.ps_ is not nullptr, copy the underlying string
+	std::string* newp = rhp.ps_ ? (new std::string(*rhp.ps_)) : nullptr;
 
 	// free the old memory
 	delete ps_;
 
-	// copy data from rhs into this object
+	// copy data from rhp into this object
 	ps_ = newp;
 	d_ = rhp.d_;
 
@@ -119,12 +100,15 @@ int main()
 	hp1 = hp1;
 	HasPtr hp3(hp2);
 	HasPtr hp4(std::move(hp1));
-
 	hp1 = hp2;
 	hp1 = std::move(hp4);
+	HasPtr hp5(hp4);
 	hp3 = hp4;
 	hp4 = hp2;
-
+	HasPtr hp6("Tired");
+	HasPtr hp7("Reflection");
+	hp6 = std::move(hp6);
+	hp7 = hp6;
 	Foo f1, f2;
 	Swap(f1, f2);
 	return 0;
