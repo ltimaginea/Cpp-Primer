6dd3d800468fa8d447d76d5e2d6e68337e607654
ltimaginea <ltimaginea@163.com>
2022-01-26 12:38:37 +0800

    Never_Polymorphism_Array

---
+++ CppPrimer/Content/Ch15_ObjectOrientedProgramming/Ch15_08_Never_Polymorphism_Array.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+class Base
+{
+public:
+	virtual ~Base() = default;
+	int b_ = 1;
+};
+
+class Derived : public Base
+{
+public:
+	~Derived() override = default;
+	double d_ = 3.14;
+	std::string str_ = "Derived";
+};
+
+int main()
+{
+	constexpr int kSize = 10;
+
+	Derived d[kSize];
+	// Evil: d decays to &d[0] which is converted to a Base*
+	Base* ptr1 = d;
+	for (int i = 0; i < kSize; ++i)
+	{
+		// Error: Subscripting the resulting base pointer will lead to invalid object access and probably to memory corruption.
+		std::cout << ptr1[i].b_ << std::endl;
+		// Error: Subscripting the resulting base pointer will lead to invalid object access and probably to memory corruption.
+		ptr1[i].b_ = 2;
+	}
+
+	Base* ptr2 = new Derived[kSize];
+	// Error: Subscripting the resulting base pointer will lead to invalid object access and probably to memory corruption.
+	delete[] ptr2;
+	ptr2 = nullptr;
+
+	return 0;
+}
