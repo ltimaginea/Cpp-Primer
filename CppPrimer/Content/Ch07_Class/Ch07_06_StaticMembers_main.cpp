4f5d156cb8631d0522bbc9151a54f544c5b14779
ltimaginea <ltimaginea@163.com>
2022-03-04 22:17:05 +0800

    static members

---
+++ CppPrimer/Content/Ch07_Class/Ch07_06_StaticMembers_main.cpp
@@ -0,0 +1,18 @@
+﻿#include "Ch07_06_StaticMembers.h"
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+	using staticmembers::Account;
+
+	Account ac;
+	auto ac_ptr = std::make_unique<Account>();
+	double r = Account::GetRate();
+	r = ac.GetRate();
+	r = ac_ptr->GetRate();
+	std::cout << sizeof(Account) << std::endl;
+
+	return 0;
+}
