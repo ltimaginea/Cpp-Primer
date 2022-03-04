a14e7d8ecaa38553dd8cb236d19ad848fcfd1d65
ltimaginea <ltimaginea@163.com>
2022-03-04 22:16:09 +0800

    static members

---
+++ CppPrimer/Content/Ch07_Class/Ch07_06_StaticMembers.h
@@ -0,0 +1,24 @@
+#ifndef CPPPRIMER_CONTENT_CH07_CLASS_CH07_06_STATICMEMBERS_H_
+#define CPPPRIMER_CONTENT_CH07_CLASS_CH07_06_STATICMEMBERS_H_
+
+#include <string>
+
+namespace staticmembers
+{
+	class Account
+	{
+	public:
+		void Calculate() { amount_ += amount_ * interest_rate_; }
+		static double GetRate() { return interest_rate_; }
+		static void SetRate(double);
+	private:
+		static double InitRate();
+		std::string owner_;
+		double amount_;
+		static double interest_rate_;
+		static constexpr int period_ = 30;
+		double daily_tbl[period_];
+	};
+}
+
+#endif // !CPPPRIMER_CONTENT_CH07_CLASS_CH07_06_STATICMEMBERS_H_
