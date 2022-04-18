1816ff3d8e53a12b0749b0afae846818a5dd1b9c
ltimaginea <ltimaginea@163.com>
2022-04-18 21:46:30 +0800

    pointer to implementation

--- CppPrimer/Content/Ch13_CopyControl/Ch13_00_Pimpl/Ch13_00_pimpl_widget.cpp
+++ CppPrimer/Content/Ch13_CopyControl/Ch13_00_Pimpl/Ch13_00_pimpl_widget.cpp
@@ -1,17 +1,36 @@
-﻿#include <iostream>
+﻿#include "Ch13_00_pimpl_widget.h"
+
+#include <iostream>
 #include <string>
 #include <vector>
 #include <utility>
 
-#include "Ch13_00_pimpl_widget.h"
 //#include "gadget.h"
 
 class Widget::Impl
 {
 public:
-	Impl(int i = 0) : i_(i) {  }
-public:
-	int GetInfo() { return i_; }
+	Impl() : i_(0) {  }
+	explicit Impl(int i) : i_(i) {  }
+
+	void Draw(const Widget& w)
+	{
+		if (w.Shown()) // this call to public member function requires the back-reference
+		{
+			std::cout << "drawing a non-const widget " << i_ << std::endl;
+		}
+	}
+
+	void Draw(const Widget& w) const
+	{
+		if (w.Shown()) // this call to public member function requires the back-reference
+		{
+			std::cout << "drawing a const widget " << i_ << std::endl;
+		}
+	}
+
+	void PrintInfo() const { std::cout << i_ << std::endl; }
+
 private:
 	std::string name_;
 	std::vector<double> data_;
@@ -19,12 +38,18 @@ private:
 	// ...
 };
 
+Widget::Widget() = default;
+//Widget::Widget() : pimpl_(std::make_unique<Impl>(0))
+//{
+//
+//}
+
 Widget::Widget(int i) : pimpl_(std::make_unique<Impl>(i))
 {
 
 }
 
-Widget::Widget(const Widget& rp) : pimpl_(rp.pimpl_ ? std::make_unique<Impl>(*rp.pimpl_) : nullptr)
+Widget::Widget(const Widget & rhs) : pimpl_(rhs.pimpl_ ? std::make_unique<Impl>(*rhs.pimpl_) : nullptr)
 {
 
 }
@@ -34,9 +59,9 @@ Widget::Widget(const Widget& rp) : pimpl_(rp.pimpl_ ? std::make_unique<Impl>(*rp
 // 所以编译器为使移动构造函数的声明和实现的“异常说明”一致，此例的合成移动构造函数将不会是 noexcept 。这算是Pimpl技术的一个缺点吧。
 Widget::Widget(Widget&&) = default;
 
-Widget& Widget::operator=(const Widget& rp)
+Widget& Widget::operator=(const Widget & rhs)
 {
-	pimpl_ = rp.pimpl_ ? std::make_unique<Impl>(*rp.pimpl_) : nullptr;
+	pimpl_ = rhs.pimpl_ ? std::make_unique<Impl>(*rhs.pimpl_) : nullptr;
 	return *this;
 }
 
@@ -47,9 +72,36 @@ Widget& Widget::operator=(Widget&&) = default;
 
 Widget::~Widget() = default;
 
-void Widget::PrintInfo()
+void Widget::Draw()
 {
-	std::cout << pimpl_->GetInfo() << std::endl;
+	if (pimpl_) // null pointer check
+	{
+		pimpl_->Draw(*this);
+	}
+}
+
+void Widget::Draw() const
+{
+	if (pimpl_) // null pointer check
+	{
+		// explicitly add low-level constness by const_cast
+		const_cast<const Impl&>(*pimpl_).Draw(*this);
+
+		// or:
+		// const_cast<const Impl*>(pimpl_.get())->Draw(*this);
+	}
+}
+
+void Widget::PrintInfo() const
+{
+	if (pimpl_) // null pointer check
+	{
+		// explicitly add low-level constness by const_cast
+		const_cast<const Impl&>(*pimpl_).PrintInfo();
+
+		// or:
+		// const_cast<const Impl*>(pimpl_.get())->PrintInfo();
+	}
 }
 
 // Tips:
